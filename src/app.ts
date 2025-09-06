9ebaa4d55ddfc7a664cc9ac69820cbb84cc59ed4
phxgg <45177754+phxgg@users.noreply.github.com>
2025-09-06 23:58:33 +0300

    refactor: move test routes and minor changes in env validation (#9)

--- src/app.ts
+++ src/app.ts
@@ -7,21 +7,16 @@ import cors from 'cors';
 import express from 'express';
 import session from 'express-session';
 import helmet from 'helmet';
-import { StatusCodes } from 'http-status-codes';
 import morgan from 'morgan';
 import passport from 'passport';
 
 import logger from '@/winston.logger';
 import { connectMongo } from '@/db';
 
-import { EventSubscriptionMethod } from './KickAPI/services/EventsService';
 import { createWebhookRouter } from './KickAPI/webhooks/WebhookRouter';
-import { attachKickClientToReq } from './middleware/attach-kick-client-to-req.middleware';
-import { bearerAuthMiddleware } from './middleware/bearer-auth.middleware';
-import { validateData } from './middleware/validate-data.middleware';
 import { createOAuthRouter } from './routers/oauth.router';
+import { createTestRouter } from './routers/test.router';
 import { initKickPassportOAuthStrategy } from './strategies/kick.strategy';
-import { testValidator } from './validators/test.validator';
 
 morgan.token('remote-user', (req: any) => {
   return req.user ? req.user.email : 'guest';
@@ -85,52 +80,8 @@ app.use(passport.session());
 // Initialize certain routes before middleware
 app.use('/webhooks', createWebhookRouter()); // kick webhooks
 app.use('/oauth', createOAuthRouter());
-
-/**
- * DEBUG
- * These endpoints are meant for debugging purposes only.
- */
-app.post('/test', bearerAuthMiddleware, validateData(testValidator), attachKickClientToReq, async (req, res) => {
-  res.json({ message: 'Data is valid', data: req.body });
-});
-
-app.get('/events', bearerAuthMiddleware, attachKickClientToReq, async (req, res) => {
-  if (!req.kick) {
-    return res.sendStatus(StatusCodes.FORBIDDEN);
-  }
-  const events = await req.kick.events.fetch();
-  res.json(events);
-});
-
-app.get('/subscribe', bearerAuthMiddleware, attachKickClientToReq, async (req, res) => {
-  if (!req.kick) {
-    return res.sendStatus(StatusCodes.FORBIDDEN);
-  }
-  const me = await req.kick.users.me();
-  const subscription = await req.kick.events.subscribe({
-    broadcasterUserId: me.userId,
-    event: { name: 'chat.message.sent', version: 1 },
-    method: EventSubscriptionMethod.WEBHOOK,
-  });
-  if (subscription.error) {
-    logger.error(`Failed to subscribe to event ${subscription.name} - ERROR_MSG: ${subscription.error}`);
-    return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
-  }
-  logger.info(subscription);
-  return res.json(subscription);
-});
-
-app.get('/delete', bearerAuthMiddleware, attachKickClientToReq, async (req, res) => {
-  if (!req.kick) {
-    return res.sendStatus(StatusCodes.FORBIDDEN);
-  }
-  const subscriptions = await req.kick.events.fetch();
-  await req.kick.events.unsubscribeMultiple(subscriptions.map((sub) => sub.id));
-  res.sendStatus(StatusCodes.NO_CONTENT);
-});
-/**
- * END DEBUG
- */
+// Test route to verify auth and validation
+app.use('', createTestRouter());
 
 // Connect to MongoDB
 // Start only after DB connect (optional: remove await to start immediately)
