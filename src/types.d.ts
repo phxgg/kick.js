0ce7e42ccb5e60945e41d2a8d42930181c256a33
phxgg <45177754+phxgg@users.noreply.github.com>
2025-09-06 23:51:02 +0300

    feat: validate env variables (#8)
    * feat: validate env variables
    
    * chore: move NodeJS namespace to top in types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import type { KickClient } from '@/KickAPI/KickClient';
-
-import type { IUser } from './models/User';
+import type { IUser } from '@/models/User';
+import type { EnvSchema } from '@/validators/env.validator';
 
 declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends EnvSchema {}
+  }
   namespace Express {
     interface User extends IUser {}
     interface Request {
