ecdd72cdf801199d45625aad1d9e4b1e15ff7413
phxgg <stamatis.1702@gmail.com>
2025-09-06 23:01:21 +0300

    refactor: use http-status-codes

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 import { UserModel } from '@/models/User';
 
@@ -14,25 +15,27 @@ export async function authMiddleware(req: Request, res: Response, next: NextFunc
 
   if (authorization) {
     const parts = authorization.split(' ');
-    if (parts.length !== 2) return res.status(401).json({ error: 'malformed_authorization_header' });
+    if (parts.length !== 2)
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'malformed_authorization_header' });
     const scheme = parts[0];
     const credentials = parts[1];
-    if (!/^Bearer$/i.test(scheme)) return res.status(401).json({ error: 'malformed_authorization_header' });
+    if (!/^Bearer$/i.test(scheme))
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'malformed_authorization_header' });
     token = credentials;
   }
 
   if (req.body && req.body.access_token) {
-    if (token) return res.status(400).json({ error: 'multiple_tokens_provided' });
+    if (token) return res.status(StatusCodes.BAD_REQUEST).json({ error: 'multiple_tokens_provided' });
     token = req.body.access_token;
   }
 
   if (req.query && req.query.access_token) {
-    if (token) return res.status(400).json({ error: 'multiple_tokens_provided' });
+    if (token) return res.status(StatusCodes.BAD_REQUEST).json({ error: 'multiple_tokens_provided' });
     token = req.query.access_token as string;
   }
 
   if (!token) {
-    return res.status(401).json({ error: 'no_token_provided' });
+    return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'no_token_provided' });
   }
 
   try {
@@ -42,6 +45,6 @@ export async function authMiddleware(req: Request, res: Response, next: NextFunc
     req.user = user;
     next();
   } catch {
-    return res.status(401).json({ error: 'invalid_token' });
+    return res.status(StatusCodes.FORBIDDEN).json({ error: 'invalid_token' });
   }
 }
