a773fff52e8d01a9e69cdaead816db6ca4e32ac3
phxgg <stamatis.1702@gmail.com>
2025-11-26 12:33:29 +0200

    feat: dummy webhook event handlers

---
+++ src/KickAPI/webhooks/v1/payloads/index.ts
@@ -0,0 +1,21 @@
+import type { ChannelFollowedEventPayload } from './ChannelFollowedEvent';
+import type { ChannelSubscriptionGiftsEventPayload } from './ChannelSubscriptionGifts';
+import type { ChannelSubscriptionNewEventPayload } from './ChannelSubscriptionNew';
+import type { ChannelSubscriptionRenewalEventPayload } from './ChannelSubscriptionRenewal';
+import type { ChatMessageSentEventPayload } from './ChatMessageSent';
+import type { KicksGiftedEventPayload } from './KicksGifted';
+import type { LivestreamMetadataUpdatedEventPayload } from './LivestreamMetadataUpdated';
+import type { LivestreamStatusUpdatedEventPayload } from './LivestreamStatusUpdated';
+import type { ModerationBannedEventPayload } from './ModerationBanned';
+
+export {
+  ChannelFollowedEventPayload,
+  ChannelSubscriptionGiftsEventPayload,
+  ChannelSubscriptionNewEventPayload,
+  ChannelSubscriptionRenewalEventPayload,
+  ChatMessageSentEventPayload,
+  KicksGiftedEventPayload,
+  LivestreamMetadataUpdatedEventPayload,
+  LivestreamStatusUpdatedEventPayload,
+  ModerationBannedEventPayload,
+};
