7ca01554db89c66976d4412dabbea8d85e46b4b0
phxgg <45177754+phxgg@users.noreply.github.com>
2025-12-15 15:21:47 +0200

    fix/chore: cron jobs throwing `ERR_UNSUPPORTED_ESM_URL_SCHEME`, bump deps, ignore `@types/node` from ncu scripts (#27)
    * chore: ignore @types/node from ncu scripts
    
    * chore: bump deps
    
    * chore: regenerate package-lock.json
    
    * fix: cron jobs init throwing ERR_UNSUPPORTED_ESM_URL_SCHEME
    
    * chore: bump deps
    
    * chore: bump deps

--- src/cron-jobs.ts
+++ src/cron-jobs.ts
@@ -1,17 +1,21 @@
 import fs from 'fs';
 import path from 'path';
+import { pathToFileURL } from 'url';
 
-export function initCronJobs() {
+export async function initCronJobs() {
   // import and initialize all cron jobs from the jobs directory
   const jobsDir = path.join(__dirname, 'jobs');
   // Dynamically import all '*.job.js' files in the jobs directory
-  fs.readdirSync(jobsDir).forEach(async (file) => {
+  const files = fs.readdirSync(jobsDir);
+
+  for (const file of files) {
     if (file.endsWith('.job.js') || file.endsWith('.job.ts')) {
-      const module = await import(path.join(jobsDir, file));
+      const filePath = path.join(jobsDir, file);
+      const module = await import(pathToFileURL(filePath).href);
       // for each imported module, if it has a default export and that export has a 'start' method, call it
       if (module.default && typeof module.default.start === 'function') {
         module.default.start();
       }
     }
-  });
+  }
 }
